5b2f1c4919546489ad704700f5cb07ba6de46757
nicolas.1im9.09 <nicolas.1im9.09@gmail.com>
2025-07-28 17:47:48 -0600

    fix: update discoverNew message to Spanish

--- src/containers/LearnerDashboardHeader/messages.js
+++ src/containers/LearnerDashboardHeader/messages.js
@@ -58,7 +58,7 @@ const messages = defineMessages({
   },
   discoverNew: {
     id: 'learnerVariantDashboard.discoverNew',
-    defaultMessage: 'Discover New',
+    defaultMessage: 'Hola soy un minion',
     description: 'Header link for switching to discover page.',
   },
   logoAltText: {
