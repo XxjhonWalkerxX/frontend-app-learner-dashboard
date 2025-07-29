ff5fdcd4c92fc5eb2e14e81881c8da2547170788
Luis Abonce <luis.torresa@nube.sep.gob.mx>
2025-07-29 13:38:35 -0600

    fix: ajustar ancho del menú desplegable y eliminar color de fondo en 'learner-variant-header'

--- src/containers/LearnerDashboardHeader/index.jsx
+++ src/containers/LearnerDashboardHeader/index.jsx
@@ -30,7 +30,6 @@ export const LearnerDashboardHeader = () => {
     <>
       <ConfirmEmailBanner />
       <Header
-        className="learner-variant-header"
         mainMenuItems={learnerHomeHeaderMenu.mainMenu}
         secondaryMenuItems={learnerHomeHeaderMenu.secondaryMenu}
         userMenuItems={learnerHomeHeaderMenu.userMenu}
