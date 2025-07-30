b261a30c8517171a4226de5618392b73fe096e0a
Luis Abonce <luis.torresa@nube.sep.gob.mx>
2025-07-30 10:48:11 -0600

    fix: eliminar la columna de la barra lateral en DashboardLayout para simplificar la estructura

--- src/containers/Dashboard/DashboardLayout.jsx
+++ src/containers/Dashboard/DashboardLayout.jsx
@@ -40,9 +40,6 @@ export const DashboardLayout = ({ children }) => {
         <Col {...courseListColumnProps} className="course-list-column">
           {children}
         </Col>
-        <Col {...columnConfig.sidebar} className="sidebar-column">
-          {!isCollapsed && (<h2 className="course-list-title">&nbsp;</h2>)}
-        </Col>
       </Row>
     </Container>
   );
