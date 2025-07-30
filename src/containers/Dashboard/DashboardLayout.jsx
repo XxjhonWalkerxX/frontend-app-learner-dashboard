57771e42b8ea6f2908b3d2a798cafc69958e9a66
Luis Abonce <luis.torresa@nube.sep.gob.mx>
2025-07-30 10:45:00 -0600

    fix: eliminar el componente WidgetSidebarSlot del DashboardLayout para simplificar la estructura

--- src/containers/Dashboard/DashboardLayout.jsx
+++ src/containers/Dashboard/DashboardLayout.jsx
@@ -42,7 +42,6 @@ export const DashboardLayout = ({ children }) => {
         </Col>
         <Col {...columnConfig.sidebar} className="sidebar-column">
           {!isCollapsed && (<h2 className="course-list-title">&nbsp;</h2>)}
-          <WidgetSidebarSlot />
         </Col>
       </Row>
     </Container>
